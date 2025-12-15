ee47010e2031e31d9a4731f841e99cdf3c99cc11
xiaoqing <luxi.xqchen@outlook.com>
2025-12-15 15:56:05 +0800

    Base framwork from zhangming

---
+++ app/src/main/java/com/example/enjoytime/SectionTwoActivity.java
@@ -0,0 +1,12 @@
+package com.example.enjoytime;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SectionTwoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_two);
+    }
+}
